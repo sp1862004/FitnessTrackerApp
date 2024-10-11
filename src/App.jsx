63fbf81c59965e95006f4f3ff3085b71e0bc8c14
shailesh patel <sp1862004@gmail.com>
2024-10-11 22:46:47 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './Layout/Header.jsx';
 import Home from './pages/Home/Home.jsx';
 
-import Index from './PAGES/Write/Index.jsx';
+
 import Update from './PAGES/Write/Update.jsx';
 import Footer from './Layout/Footer.jsx';
 import SignIn from './PAGES/Home/Signin.jsx';
@@ -12,6 +12,7 @@ import SignUp from './PAGES/Home/Signup.jsx';
 import ContactUs from './PAGES/Home/Contect.jsx';
 import AddProject from './PAGES/Write/AddProject.jsx';
 import ShowMorePage from './pages/Home/ShowMorePage.jsx';
+import Index from './PAGES/Write/Index.jsx';
 
 
 function App() {
